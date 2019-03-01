ed810f9d914ff008dc2a4a90f03ddc29e6f413a1
Alex Thacker <thacker.alex@yahoo.com>
2019-03-01 10:07:50 -0600

    added objects and array to push content into arrays

---
+++ src/scripts/journal.js
@@ -0,0 +1,26 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const objectsJournalEntry = {
+    date: '3-1-19',
+    concepts: 'lab day',
+    entry:'lab day to work on exercises',
+    mood: 'happy'
+};
+
+const objectsJournalEntry1 = {
+    date: '2-28-19',
+    concepts: 'functions, strings, loops',
+    entry: 'the concept of functions was pretty straight forward, but creating them and using them in line with loops and conditional statements was more of a challenge',
+    mood: 'aight'
+
+}
+
+
+const entryAll = [];
+
+entryAll.push(objectsJournalEntry);
+entryAll.push(objectsJournalEntry1);
+console.log(entryAll);
+
